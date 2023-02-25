256b013d6824e2bfc312566601330cb5df886341
kotpilun <kotpilun3m@gmail.com>
2023-02-25 08:54:44 +0300

    extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  str = String(str);
+  let repeatTimes = 1;
+  let separator = '+';
+  let addition = '';
+  let additionRepeatTimes = 1;
+  let additionSeparator = '|';
+  let additionStr = '';
+  if ('repeatTimes' in options) repeatTimes = options.repeatTimes;
+  if ('separator' in options) separator = options.separator;
+  if ('addition' in options) addition = String(options.addition);
+  if ('additionRepeatTimes' in options) additionRepeatTimes = options.additionRepeatTimes;
+  if ('additionSeparator' in options) additionSeparator = options.additionSeparator;
+  
+  const sepLength = separator.length; 
+  const addSepLength = additionSeparator.length;
+  if (addition != '') {
+    additionStr = `${addition}${additionSeparator}`.repeat(additionRepeatTimes).slice(0, -addSepLength);
+  }
+  const resStr = `${str}${additionStr}${separator}`.repeat(repeatTimes).slice(0, -sepLength);
+  return resStr;
 }
+
+// console.log(
+//   repeater(null, { repeatTimes: 3, separator: '??? ', addition: null, additionRepeatTimes: 3, additionSeparator: '!!!' })
+// );
+// // , 'nullnull!!!null!!!null??? nullnull!!!null!!!null??? nullnull!!!null!!!null'
+
 
 module.exports = {
   repeater
